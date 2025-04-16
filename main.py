0df44747a80fa8ee7799213757a0f48cdf469425
parkerbrock <pbrock@ufl.edu>
2025-04-16 12:04:30 -0400

    somewhat working

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("EPL_Data.csv")
 print(data.head())
 
+plt.figure(figsize=(10,6))
 plt.scatter(data["Net_transfers"], data["Points"])
+for i, row in data.iterrows():
+    plt.text(row["Net_transfers"] + 1, row["Points"], row["Team"])
 plt.xlabel("Transfer Spend (€M)")
 plt.ylabel("League Points")
+plt.title("EPL 23-24")
 plt.grid(True)
 plt.show()
